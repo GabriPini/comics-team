484de106a0d9d496f027bddf0d9b4b88bea5f32a
GabriPini <lele19977@gmail.com>
2022-06-20 18:05:57 +0200

    fix2

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Route::get('/guest/characters', 'Guest\CharacterController@index')->name('charac
 Route::get('/guest/characters/{character}', 'Guest\CharacterController@show')->name('characters.show');
 
 Route::get('/', function () {
-    $comic_list = config('comic_list');
-    return view('comics', ['comic_list' => $comic_list] );
+    $comic_list = config('db');
+    return view('comics', compact('comic_list'));
 })->name('comics');
 
 Route::get('/movies', function () {
@@ -58,7 +58,7 @@ Route::get('/shop', function () {
 
 // comic single page
 Route::get('/comics/{id}', function ($id) {
-    $comic_list = config('comic_list');
+    $comic_list = config('db');
 
     if(is_numeric($id) && ($id < count($comic_list) && $id >= 0)){
 
